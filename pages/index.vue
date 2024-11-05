300f1bdd48557ea58f50df5a2f4b912c4d89ff13
Han Wang <hanforwork896@gmail.com>
2024-11-05 23:27:51 +0800

    feat: add BS5 in project (Day02 daily task)

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,22 @@
 
 <template>
   <div>
-    Page: 首頁
+    <p class="title">Page: 首頁</p>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+$primary: #0d6efd;
+.title {
+  color: $primary;
+}
+</style>
