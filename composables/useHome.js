55b790d11df0a42f2ac0b20fbc4ce1c844cf97e7
Han Wang <hanforwork896@gmail.com>
2024-11-11 20:55:53 +0800

    feat: add composables (Day05 daily task)

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,3 +1,28 @@
 export const useHome = () => {
-  return {};
+  const newsList = ref([]);
+  const isLoading = ref(false);
+
+  // API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
+  // 使用 ES6 fetch() 或是 axios.get() 串接 API
+  // 切換 isLoading 狀態
+  async function getData() {
+    const res = await fetch("https://nuxr3.zeabur.app/api/v1/home/news/");
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    try {
+      const data = await res.json();
+      newsList.value = data.result;
+      isLoading.value = true;
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+  return {
+    newsList,
+    isLoading,
+    getData,
+  };
 };
